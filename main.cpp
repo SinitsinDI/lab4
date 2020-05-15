6d61d1ba681c5204393d464c7f452e9c4e9c89b8
Daniel(SinitsinDI) <daniel@example.com>
2020-05-15 20:20:29 +0300

    new: подключил curl

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include "histogram.h"
 #include "svg.h"
-
+#include <curl/curl.h>
 using namespace std;
 Input
 read_input(istream& in, bool prompt)
@@ -25,7 +25,7 @@ read_input(istream& in, bool prompt)
 }
 int
 main() {
-
+    curl_global_init(CURL_GLOBAL_ALL);
     const auto input = read_input(cin, true);
     const auto bins = make_histogram(input);
     show_histogram_svg(bins);
