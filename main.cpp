97f144366be1295c6edd74e2114d2dcc43f5278c
Daniel(SinitsinDI) <daniel@example.com>
2020-05-15 20:47:26 +0300

    code: пункт 3,2,6 выполнен

--- main.cpp
+++ main.cpp
@@ -27,7 +27,14 @@ int main(int argc, char* argv[])
 {
     if (argc>1)
     {
-        cout<<"argv[0]="<<argv[0];
+        CURL *curl = curl_easy_init();
+        if(curl)
+        {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+        }
         return(0);
     }
     else
