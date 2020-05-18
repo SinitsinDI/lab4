4eb568f268b0296b7c03bd6aca5fd444c4341c43
Daniel(SinitsinDI) <daniel@example.com>
2020-05-18 17:40:48 +0300

    code: 3.1 Commander Shepard

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cstdio>
 #include "histogram.h"
 #include "svg.h"
 using namespace std;
@@ -7,6 +8,11 @@ using namespace std;
 
 int main()
 {
+    const char* name = "Commander Shepard";
+    int year = 2154;
+    printf("%s was born in %d.\n", name, year);
+    printf("n = %08x\n", 0x1234567);
+    return 0;
 
     size_t number_count;
     cerr << "Enter number count: ";
