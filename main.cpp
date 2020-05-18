0f18a8a53e4194a6959678653733c7038b286bb6
Daniel(SinitsinDI) <daniel@example.com>
2020-05-18 18:20:08 +0300

    code: Получена версия Windows

--- main.cpp
+++ main.cpp
@@ -9,10 +9,26 @@ using namespace std;
 int main()
 {
 
-    printf("WinVersion  %x\n", GetVersion());
-    printf("WinVersion  %u\n", GetVersion());
+  //  printf("WinVersion  %x\n", GetVersion());
+  //  printf("WinVersion  %u\n", GetVersion());
+    DWORD mask = 0x0000ffff;
+    DWORD mask_major = 0b00000000'00000000'00000000'11111111;
+    DWORD info = GetVersion();
+    DWORD platform = info >> 16;
+    DWORD version = info & mask;
+    DWORD version_major = version & mask_major;
+    DWORD version_minor = version >> 8;
+    printf("Windows (decimal) version is %u.\n", version);
+    printf("Windows (16x) version is %x.\n", version);
+    printf("Your platform is %u.\n", platform);
+    printf("Windows major version is %u.\n", version_major);
+    printf("Windows minor version is %u.\n", version_minor);
 
-    size_t number_count;
+    if ((info & 0x40000000) == 0) {
+        DWORD build = platform;
+        printf("Windows build is %u.\n", build);
+    }
+   /* size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
 
@@ -27,6 +43,6 @@ int main()
     find_minmax(numbers,min,max);
 
     const auto bins = make_histogram(numbers, bin_count,min,max);
-    show_histogram_svg(bins);
+    show_histogram_svg(bins);*/
     return 0;
 }
